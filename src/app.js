ace94bcc3e2335ead43d29e2a351a15ad6340f80
Jatin Tyagi <jatintyagi@Mac.lan>
2026-01-19 02:27:25 +0530

    handling mulitple route handler

--- src/app.js
+++ src/app.js
@@ -2,12 +2,30 @@ const express = require('express');
 
 const app = express();
 
+
+app.use('/user', (req, res, next) => {
+    console.log("Route 1")
+    next();
+},
+[(req, res, next) => {
+    console.log("Route 2")
+    next();
+},
+(req, res, next) => {
+    console.log("Route 3")
+    next();
+}],(req, res, next) => {
+    console.log("Route 4")
+    res.send('4th route')
+}
+);
+
 //query params
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-    //console.log(req.params);
-    res.send({ user: "Jatin Tyagi", age: "27", location: "Delhi" });
-});
+// app.get("/user/:userId/:name/:password", (req, res) => {
+//     //console.log(req.params);
+//     res.send({ user: "Jatin Tyagi", age: "27", location: "Delhi" });
+// });
 
 
 app.listen(3000, () => {
